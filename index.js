f03ab45103c51a6aee10209b96d515ed039f17f7
Julien Broens <julien-broens@MBP-de-Julien.lan>
2020-08-14 17:03:25 +0200

    add exo-03

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ import Vue from '/node_modules/vue/dist/vue';
 const app = new Vue({
     el: '#app',
     data: {
-        costOfApple: 10,
-        costOfTomatos: 5,
-        costOfBanana: 8
+        message: "Mon application VueJS",
+        shoppingCart: [{
+                label: 'Pommes',
+                cost: 6,
+                url: "#"
+            },
+            {
+                label: 'Bananes',
+                cost: 2,
+                url: "#"
+            },
+            {
+                label: 'Noix de coco',
+                cost: 8,
+                url: "#"
+            }
+        ]
     },
     computed: {
         totalPrice() {
-            return this.costOfApple + this.costOfTomatos + this.costOfBanana
+            let total = 0;
+            this.shoppingCart.forEach(element => {
+                total += element.cost
+            });
+            return total
         }
     }
 })
