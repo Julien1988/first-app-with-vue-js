de06d19c7ad06d534ac0491db439930e333e8354
Julien Broens <julien-broens@MBP-de-Julien.lan>
2020-08-14 16:49:49 +0200

    add exo-02

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ import Vue from '/node_modules/vue/dist/vue';
 const app = new Vue({
     el: '#app',
     data: {
-        message: "Bonjour",
-        myArray: ['Une entrée', 'Deuxièm etrée']
+        costOfApple: 10,
+        costOfTomatos: 5,
+        costOfBanana: 8
+    },
+    computed: {
+        totalPrice() {
+            return this.costOfApple + this.costOfTomatos + this.costOfBanana
+        }
     }
 })
 
